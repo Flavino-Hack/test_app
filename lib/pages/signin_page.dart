4cdeabdd30ec7cb2e7c9fc33f1d50153a28c5538
Flavino-Hack <flavienkiema1@gmail.com>
2025-10-25 03:22:21 +0000

    modif de Sign In

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -8,6 +8,11 @@ class SigninPage extends StatefulWidget {
 }
 
 class _SigninPageState extends State<SigninPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _isObscure = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,9 +42,11 @@ class _SigninPageState extends State<SigninPage> {
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 20),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
+                controller: _emailController,
                 decoration: InputDecoration(
                   labelText: 'Email',
                   filled: true,
@@ -51,9 +58,21 @@ class _SigninPageState extends State<SigninPage> {
                   ),
                 ),
                 style: TextStyle(color: Colors.white),
+
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Please enter a valid email address';
+                  } else {
+                    return null;
+                  }
+                },
               ),
               SizedBox(height: 20),
               TextFormField(
+                controller: _passwordController,
+                obscureText: _isObscure,
                 decoration: InputDecoration(
                   labelText: 'Password',
                   filled: true,
@@ -63,8 +82,87 @@ class _SigninPageState extends State<SigninPage> {
                     borderRadius: BorderRadius.circular(5.0),
                     borderSide: BorderSide.none,
                   ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                    icon: Icon(
+                      _isObscure ? Icons.visibility : Icons.visibility_off,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
                 style: TextStyle(color: Colors.white),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  } else if (value.length < 8) {
+                    return 'Password must be at least 8 characters long';
+                  } else if (!RegExp(r'(?=.*[A-Z])').hasMatch(value)) {
+                    return 'Password must contain at least one uppercase letter';
+                  } else if (!RegExp(r'(?=.*[a-z])').hasMatch(value)) {
+                    return 'Password must contain at least one lowercase letter';
+                  } else if (!RegExp(r'(?=.*\d)').hasMatch(value)) {
+                    return 'Password must contain at least one number';
+                  } else if (!RegExp(r'(?=.*[!@#\$&*~])').hasMatch(value)) {
+                    return 'Password must contain at least one special character';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity,
+
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // print(_emailController.text);
+                      //print(_passwordController.text);
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  child: Text('Sign In', style: TextStyle(fontSize: 18)),
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              Text(
+                "OR",
+                style: TextStyle(color: Colors.white, fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Navigate to Sign Up page
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[700],
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                  ),
+                  child: Text('Sign Up', style: TextStyle(fontSize: 18)),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Signing in means you agree to our Terms of Service and Privacy Policy.",
+                style: TextStyle(color: Colors.white, fontSize: 16),
+                textAlign: TextAlign.center,
               ),
             ],
           ),
